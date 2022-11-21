4ee7dd20c1fb238f087059f393fcbd8670acb32a
nonconvextech <rust@ft.tech>
2022-11-22 01:51:10 +0800

    chore: bench to compare lazy and eager msg eval

---
+++ benches/format.rs
@@ -0,0 +1,38 @@
+#![feature(test)]
+extern crate test;
+use ftlog::{FtLogFormat, FtLogFormatter, Record};
+
+#[bench]
+fn format_value(b: &mut test::Bencher) {
+    let record = Record::builder()
+        .level(log::Level::Info)
+        .file_static(Some("benches/string-format.rs"))
+        .line(Some(29))
+        .build();
+    b.iter(|| {
+        FtLogFormatter.msg(&record);
+    });
+}
+
+#[bench]
+fn format_string(b: &mut test::Bencher) {
+    let format = Box::new(|record: &Record| {
+        format!(
+            "{} {}/{}:{} {}",
+            record.level(),
+            std::thread::current().name().unwrap_or_default(),
+            record.file().unwrap_or(""),
+            record.line().unwrap_or(0),
+            record.args()
+        )
+    });
+
+    let record = Record::builder()
+        .level(log::Level::Info)
+        .file_static(Some("benches/string-format.rs"))
+        .line(Some(29))
+        .build();
+    b.iter(|| {
+        format(&record);
+    });
+}
