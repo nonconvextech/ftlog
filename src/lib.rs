d025e30fe4fd44292453f40b28363aa3c43a80de
nonconvextech <rust@ft.tech>
2022-11-21 14:00:35 +0800

    perf: only flush when idle

--- src/lib.rs
+++ src/lib.rs
@@ -4,168 +4,249 @@
 //! [![Latest Version](https://img.shields.io/crates/v/ftlog.svg)](https://crates.io/crates/ftlog)
 //! [![ftlog](https://docs.rs/ftlog/badge.svg)](https://docs.rs/ftlog)
 //!
-//! 普通的日志库受到磁盘io和系统pipe影响，单线程顺序写入单条速度大概要2500ns（SSD），如果碰到io抖动或者慢磁盘，日志会是低延时交易的主要瓶颈。
-//! 本库先把日志send到channel，再启动后台单独线程recv并且磁盘写入，测试速度在300ns左右。
+//! Logging is affected by disk IO and pipe system call.
+//! Sequencial log call can a bottleneck in scenario where low
+//! latency is critical (e.g. high frequency trading).
 //!
-//! **CAUTION**: this crate use `unchecked_math` unstable feature and `unsafe` code. Only use this crate in rust `nightly` channel.
+//! `ftlog` alleviates this bootleneck by sending message to dedicated logger
+//! thread, and computing as little as possible in main/worker thread.
 //!
-//! ### 日志格式
-//! ```plain
-//! 2022-04-08 19:20:48.190+08 298ms <<这里是日志的延时，正常这个数为0，如果太大说明channel堵塞了 INFO main/src/ftlog.rs:14 14575
-//! ```
+//! `ftlog` can boost log performance by **times** in main/worker thread. See
+//! performance for details.
 //!
-//! ```plain
-//! 2022-04-10 21:27:15.996+08 0ms 2 <<这表示丢弃了2条日志，说明使用了limit语法 INFO main/src/main.rs:29 limit running3 !
-//! ```
+//! **CAUTION**: this crate use `unchecked_math` unstable feature and `unsafe`
+//! code. Only use this crate in rust `nightly` channel.
 //!
-//! ## 配置
-//! 加入引用
+//!
+//! # Usage
+//!
+//! Add to your `Cargo.toml`:
+//!
 //! ```toml
 //! ftlog = "0.2.0"
 //! ```
 //!
-//! 在main开头加入配置：
+//! Configure and initialize ftlog at the start of your `main` function:
 //! ```rust
-//! // 最简配置用法，使用默认配置，输出到stderr
-//! ftlog::builder().build().unwrap().init().unwrap();
+//! // ftlog re-export `log`'s macros, so no need to add `log` to dependencies
+//! use ftlog::{debug, trace};
+//! use log::{error, info, warn};
 //!
+//! fn main() {
+//!     // minimal configuration with default setting
+//!     // define root appender, pass None would write to stderr
+//!     let dest = FileAppender::new("./current.log");
+//!     ftlog::builder().root(dest).build().unwrap().init().unwrap();
+//!
+//!     trace!("Hello world!");
+//!     debug!("Hello world!");
+//!     info!("Hello world!");
+//!     warn!("Hello world!");
+//!     error!("Hello world!");
+//!
+//!     // when main thread is done, logging thread may be busy printing messages
+//!     // wait for log output to flush, otherwise messages in memory yet might lost
+//!     ftlog::logger().flush();
+//! }
 //! ```
 //!
-//! 默认的配置会把日志输出到stderr，程序启动的时候要执行bin/yourjob &>> logs/server.log来把日志定向到文件。（注意shell要用/bin/bash不要用/bin/sh。/bin/sh在ubuntu下有问题，不标准)
-//!
-//! 也可以直接输出到固定文件，完整的配置用法如下:
+//! A more complicated but feature rich usage:
 //!
 //! ```rust,no_run
 //! use ftlog::{appender::{Period, FileAppender}, LevelFilter, Record, FtLogFormat};
 //!
-//! // 完整用法
-//!
-//! // Custom format
-//!
-//! // Here is the dumbest implementation that format to String in worker thread
-//! // A better way is to store required field and send to log thread to format into string
-//! struct StringFormatter;
-//! impl FtLogFormat for StringFormatter {
+//! // Custom formatter
+//! // A formatter defines how to build a message.
+//! // Since Formatting message into string can slow down the log macro call,
+//! // the idomatic way is to send required field as is to log thread, and build message in log thread.
+//! struct MyFormatter;
+//! impl FtLogFormat for MyFormatter {
 //!     fn msg(&self, record: &Record) -> Box<dyn Send + Sync + std::fmt::Display> {
-//!         Box::new(format!(
-//!             "{} {}/{}:{} {}",
-//!             record.level(),
-//!             std::thread::current().name().unwrap_or_default(),
-//!             record.file().unwrap_or(""),
-//!             record.line().unwrap_or(0),
-//!             record.args()
+//!         Box::new(Msg {
+//!             level: record.level(),
+//!             thread: std::thread::current().name().map(|n| n.to_string()),
+//!             file: record.file_static(),
+//!             line: record.line(),
+//!             args: format!("{}", record.args()),
+//!             module_path: record.module_path_static(),
+//!         })
+//!     }
+//! }
+//!
+//! // Store necessary field, define how to build into string with `Display` trait.
+//! struct Msg {
+//!     level: Level,
+//!     thread: Option<String>,
+//!     file: Option<&'static str>,
+//!     line: Option<u32>,
+//!     args: String,
+//!     module_path: Option<&'static str>,
+//! }
+//!
+//! impl Display for Msg {
+//!     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//!         f.write_str(&format!(
+//!             "{}@{}||{}:{}[{}] {}",
+//!             self.thread.as_ref().map(|x| x.as_str()).unwrap_or(""),
+//!             self.module_path.unwrap_or(""),
+//!             self.file.unwrap_or(""),
+//!             self.line.unwrap_or(0),
+//!             self.level,
+//!             self.args
 //!         ))
 //!     }
 //! }
 //!
-//! // 配置logger
+//! // configurate logger
 //! let logger = ftlog::builder()
-//!     // 这里可以定义自己的格式，时间格式暂时不可以自定义
-//!     // 只能设置全局格式，不允许对不同输出目标单独设定格式
+//!     // use custom fotmat
+//!     // datetime format is not configurable for performance
 //!     .format(StringFormatter)
-//!     .root(FileAppender::rotate("./current.log", Period::Day))
-//!     // 将ftlog::appender下的日志输出到默认日志输出，并过滤Warn以下级别
-//!     .filter("ftlog::appender", None, LevelFilter::Warn)
-//!     // 将ftlog::appender::file下的日志输出到后面定义的"ftlog-appender"输出方式，默认过滤级别
-//!     .filter("ftlog::appender::file", "ftlog-appender", None)
-//!     // 定义新的日志输出方式，写出数据结构只要满足`Send + Write + 'static`
-//!     .appender("ftlog-appender", FileAppender::rotate("ftlog.log", Period::Day))
-//!     // 全局日志过滤等级
+//!     // global max log level
 //!     .max_log_level(LevelFilter::Info)
+//!     // define root appender, pass None would write to stderr
+//!     .root(FileAppender::rotate_with_expire(
+//!         "./current.log",
+//!         Period::Minute,
+//!         Duration::seconds(30),
+//!     ))
+//!     // ---------- configure additional filter ----------
+//!     // write to "ftlog-appender" appender, with different level filter
+//!     .filter("ftlog::appender", "ftlog-appender", LevelFilter::Error)
+//!     // write to root appender, but with different level filter
+//!     .filter("ftlog", None, LevelFilter::Trace)
+//!     // write to "ftlog" appender, with default level filter
+//!     .filter("ftlog::appender::file", "ftlog", None)
+//!     // ----------  configure additional appender ----------
+//!     // new appender
+//!     .appender("ftlog-appender", FileAppender::new("ftlog-appender.log"))
+//!     // new appender, rotate to new file every Day
+//!     .appender("ftlog", FileAppender::rotate("ftlog.log", Period::Day))
 //!     .build()
 //!     .expect("logger build failed");
-//! // 初始化
+//! // init global logger
 //! logger.init().expect("set logger failed");
 //! ```
 //!
-//! ## 用法
+//! See `examples/complex.rs`.
 //!
-//! ```rust,no_run
-//! use ftlog::{trace, debug};
-//! use log::{info, warn ,error};
-//! trace!("Hello world!");
-//! debug!("Hello world!");
-//! info!("Hello world!");
-//! warn!("Hello world!");
-//! error!("Hello world!");
-//! ```
+//! ## Default Log Format
 //!
-//! 在main最后加入flush，否则在程序结束时未写入的日志会丢失：
-//! ```rust,no_run
-//! ftlog::logger().flush();
-//! ```
+//! Datetime format is fixed for performance.
 //!
-//! 参见 `examples/ftlog.rs`
+//! > 2022-04-08 19:20:48.190+08 **298ms** INFO main/src/ftlog.rs:14 My log
+//! > message
 //!
-//! ### 带间隔的日志
-//! 本库支持受限写入的功能。
+//! Here `298ms` denotes the latency between log macros call (e.g.
+//! `log::info!("msg")`) and actual printing in log thread. Normally this will
+//! be 0ms.
+//!
+//! A large delay indicates log thread might be blocked by excessive log
+//! messages.
+//!
+//! > 2022-04-10 21:27:15.996+08 0ms **2** INFO main/src/main.rs:29 limit
+//! > running3 !
+//!
+//! The number **2** above shows how many log messages have been discarded.
+//! Only shown when limiting logging interval for a single log call (e.g.
+//! `log::info!(limit=3000;"msg")`).
+//!
+//! ## Log with interval
+//!
+//! `ftlog` allows restriction on writing frequency for single log call.
+//!
+//! If the above line is called multiple times within 3000ms, then it will only
+//! log once with a added number donates the number of discarded log message.
+//!
+//! Each log call owns independent interval, so we can set different interval
+//! for different log call. Internally, `ftlog` records last print time by a
+//! combination of (module name, file name, line of code).
+//!
+//! ### Example
 //!
 //! ```rust
 //! # use ftlog::info;
-//! info!(limit=3000; "limit running{} !", 1);
+//! info!(limit=3000; "limit running{} !", 3);
+//! info!(limit=1000; "limit running{} !", 1);
 //! ```
-//! 上面这一行日志会有最小3000毫秒的间隔，也就是最多3000ms一条。
+//! The minimal interval of the the above log call is 3000ms.
 //!
-//! 不同代码行的日志间隔是互相独立的，互不影响。
+//! ```plain
+//! 2022-04-10 21:27:15.996+08 0ms 2 INFO main/src/main.rs:29 limit running1 !
+//! ```
+//! The number **2** above shows how many log messages have been discarded.
 //!
-//! 后台线程以(模块、文件名、代码行)为key来记录上次日志打印时间，小于间隔则跳过。
+//! ## Log rotation
+//! `ftlog` supports log rotation in local timezone. The available rotation
+//! periods are:
 //!
-//! ### 日志分割
-//! 本库支持日志按时间分割，自动适配本地时区，可选分割时间精度如下：
+//! - minute `Period::Minute`
+//! - hour `Period::Hour`
+//! - day `Period::Day`
+//! - month `Period::Month`
+//! - year `Period::Year`
 //!
-//! - 分钟 `Period::Minute`
-//! - 小时 `Period::Hour`
-//! - 日 `Period::Day`
-//! - 月 `Period::Month`
-//! - 年 `Period::Year`
+//! Log rotation is configured in `FileAppender`, and datetime will be added to
+//! the end of filename:
 //!
 //! ```rust
-//! use ftlog::{appender::{Period, FileAppender}};
+//! use ftlog::appender::{FileAppender, Period};
 //!
 //! let logger = ftlog::builder()
-//!     .root(FileAppender::rotate("./current.log", Period::Minute))
+//!     .root(FileAppender::rotate("./mylog.log", Period::Minute))
 //!     .build()
 //!     .unwrap();
 //! logger.init().unwrap();
 //! ```
 //!
-//! 设置按分钟分割时，输出日志文件格式为 `mylog-{MMMM}{YY}{DD}T{hh}{mm}.log`，其中`mylog`为调用时指定的文件名。如果未指定拓展名则在指定文件名后面加时间标签。
+//! When configured to divide log file by minutes,
+//! the file name of log file is in the format of
+//! `mylog-{MMMM}{YY}{DD}T{hh}{mm}.log`. When by days, the log file names is
+//! something like `mylog-{MMMM}{YY}{DD}.log`.
 //!
-//! 时间戳只保留到分割的时间精度，如按日分割保存的文件名格式为 `log-{MMMM}{YY}{DD}.log`。
-//!
-//! 输出的日志示例：
+//! Log filename examples:
 //! ```sh
 //! $ ls
-//! # 按分钟分割
+//! # by minute
 //! current-20221026T1351.log
 //! current-20221026T1352.log
 //! current-20221026T1353.log
-//! # 按小时分割
+//! # by hour
 //! current-20221026T13.log
 //! current-20221026T14.log
-//! # 按日分割
+//! # by day
 //! current-20221026.log
 //! current-20221027.log
-//! # 按月分割
+//! # by month
 //! current-202210.log
 //! current-202211.log
-//! # 按年分割
+//! # by year
 //! current-2022.log
 //! current-2023.log
-//! # 设置的保存文件名无拓展时(如"./log")，则直接在文件名末尾加时间
+//!
+//! # omitting extension (e.g. "./log") will add datetime to the end of log filename
 //! log-20221026T1351
 //! log-20221026T1352
 //! log-20221026T1353
 //! ```
-//! #### 自动清理分割后的日志
 //!
-//! 在设置了日志按时间分割的基础上，可以设置自动清理日志。按照上次修改时间清理ftlog产生的日志文件。
-//! 注意：自动清理使用文件名匹配，文件名与日志文件名格式完全一致的文件也会被清理。
+//! ### Clean outdated logs
+//!
+//! With log rotation enabled, it is possible to clean outdated logs to free up
+//! disk space with `FileAppender::rotate_with_expire` method.
+//!
+//! `ftlog` first finds files generated by `ftlog` and cleans outdated logs by
+//! last modified time. `ftlog` find generated logs by filename matched by file
+//! stem and added datetime.
+//!
+//! **ATTENTION**: Any files that matchs the pattern will be deleted.
 //!
 //! ```rust
 //! use ftlog::{appender::{Period, FileAppender, Duration}};
 //!
-//! // 这个例子中，文件名满足current-\d{8}T\d{4}.log的文件将被清理
+//! // clean files named like `current-\d{8}T\d{4}.log`.
+//! // files like `another-\d{8}T\d{4}.log` or `current-\d{8}T\d{4}` will not be deleted, since the filenames' stem do not match.
+//! // files like `current-\d{8}.log` will remains either, since the rotation durations do not match.
 //! let appender = FileAppender::rotate_with_expire("./current.log", Period::Minute, Duration::seconds(180));
 //! let logger = ftlog::builder()
 //!     .root(appender)
@@ -173,6 +254,16 @@
 //!     .unwrap();
 //! logger.init().unwrap();
 //! ```
+//!
+//! # Performance
+//!
+//! |  | Apple M1 Pro, 3.2GHz, Rust：1.67.0-nightly |  |
+//! | ----------| --------- | ---------- |
+//! | `ftlog` output to file | 156 ns/iter (+/- 23) | 1,636 ns/iter (+/- 229) |
+//! | `env_logger` output to file | 1,636 ns/iter (+/- 229) | 1,636 ns/iter (+/- 229) |
+//! | `ftlog` output to file with `BufWriter` | 154 ns/iter (+/- 23) | 274 ns/iter (+/- 41) |
+//! | `env_logger` output to file with `BufWriter` | 274 ns/iter (+/- 41) | 607 ns/iter (+/- 41) |
+
 pub use log::{
     debug, error, info, log, log_enabled, logger, trace, warn, Level, LevelFilter, Record,
 };
@@ -546,6 +637,16 @@ impl Builder {
                             break;
                         }
                         Err(TryRecvError::Empty) => {
+                            if last_flush.elapsed() > Duration::from_millis(1000) {
+                                let flush_errors = appenders
+                                    .values_mut()
+                                    .chain([&mut root])
+                                    .filter_map(|w| w.flush().err());
+                                for err in flush_errors {
+                                    log::warn!("Ftlog flush error: {}", err);
+                                }
+                                last_flush = Instant::now();
+                            };
                             std::thread::sleep(Duration::from_micros(200));
                         }
                         Err(e) => {
@@ -555,16 +656,6 @@ impl Builder {
                             );
                         }
                     }
-                    if last_flush.elapsed() > Duration::from_millis(1000) {
-                        let flush_errors = appenders
-                            .values_mut()
-                            .chain([&mut root])
-                            .filter_map(|w| w.flush().err());
-                        for err in flush_errors {
-                            log::warn!("Ftlog flush error: {}", err);
-                        }
-                        last_flush = Instant::now();
-                    };
                 }
             })?;
         Ok(Logger {
